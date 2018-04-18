e53795b635531277967fcabfd29c87aadaa83706
Zhipeng Wen <zwen@e1z2r9p2.42.us.org>
2018-04-18 11:05:38 -0700

    fix ft_strcpy using forbidden functions

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 09:03:43 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/18 09:45:28 by zwen             ###   ########.fr       */
+/*   Updated: 2018/04/18 11:03:37 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strcpy(char *dst, const char *src)
 	register size_t	i;
 	register size_t	len;
 
-	len = strlen(src);
+	len = ft_strlen(src);
 	i = 0;
 	while (i <= len)
 	{
