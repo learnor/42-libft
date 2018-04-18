f19cc956463ccb674473f4a7f2487d8612f17da3
Zhipeng Wen <zwen@e1z2r9p2.42.us.org>
2018-04-18 09:15:46 -0700

    ft_strcpy

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/17 20:11:18 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/18 09:15:03 by zwen             ###   ########.fr       */
+/*   Created: 2018/04/18 09:03:43 by zwen              #+#    #+#             */
+/*   Updated: 2018/04/18 09:12:45 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
+#include "libft.h"
 
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
-char	*ft_strcpy(char *dst, const char *src);
-int		ft_isalpha(int c);
-void	ft_putchar(char c);
-void	ft_putstr(char const *s);
-#endif
+char	*ft_strcpy(char *dst, const char *src)
+{
+	register int	i;
+	int				len;
+
+	len = strlen(src);
+	i = -1;
+	while (++i <= len)
+		dst[i] = src[i];
+	return (dst);
+}
