f1e0eba882c29b6274c60b7bb5d2064368f8877e
Zhipeng Wen <zwen@e1z2r10p8.42.us.org>
2018-04-18 00:23:28 -0700

    ft_putstr

--- ft_putstr.c
+++ ft_putstr.c
@@ -1,20 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/17 20:11:18 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/18 00:14:06 by zwen             ###   ########.fr       */
+/*   Created: 2018/04/17 23:54:32 by zwen              #+#    #+#             */
+/*   Updated: 2018/04/18 00:01:37 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
-# include <unistd.h>
+#include "libft.h"
 
-void	ft_putchar(char c);
-void	ft_putstr(char const *s);
-#endif
+void	ft_putstr(char const *s)
+{
+	while (*s)
+		ft_putchar(*(s++));
+}
