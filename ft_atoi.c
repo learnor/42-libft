545f8b34af28945111074869a050cbd500e920af
Zhipeng Wen <zwen@e1z3r4p7.42.us.org>
2018-04-20 15:17:56 -0700

    fix ft_atoi for max positive/negative

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 00:29:12 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/20 14:48:14 by zwen             ###   ########.fr       */
+/*   Updated: 2018/04/20 15:15:45 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,24 @@ int		ft_atoi(const char *str)
 	register int		res;
 	register int		neg;
 	register const char	*s;
+	register int		tmp;
 
 	s = str;
 	while (*s && ft_is_space(*s))
 		s++;
-	neg = 0;
+	neg = 1;
 	if (*s == '+' || *s == '-')
-	{
-		if (*s == '-')
-			neg = 1;
-		s++;
-	}
+		if (*s++ == '-')
+			neg = -1;
 	res = 0;
 	while (*s >= '0' && *s <= '9')
 	{
-		if (neg)
-			res = res * 10 - (*s - '0');
-		else
-			res = res * 10 + (*s - '0');
-		s++;
+		tmp = res * 10 + neg * (*s++ - '0');
+		if (neg > 0 && tmp < res)
+			return (-1);
+		if (neg < 0 && tmp > res)
+			return (0);
+		res = tmp;
 	}
 	return (res);
 }
