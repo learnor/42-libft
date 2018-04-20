a2085267bd44aaf8ae97b3cba908f1262c427bbf
Zhipeng Wen <zwen@e1z3r3p5.42.us.org>
2018-04-20 00:28:07 -0700

    ft_strcmp

---
+++ ft_strcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/19 17:17:05 by zwen              #+#    #+#             */
+/*   Updated: 2018/04/19 23:53:04 by zwen             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	register unsigned char	*ps1;
+	register unsigned char	*ps2;
+
+	ps1 = (unsigned char *)s1;
+	ps2 = (unsigned char *)s2;
+	while (*ps1 == *ps2 && *ps1)
+	{
+		ps1++;
+		ps2++;
+	}
+	return (*ps1 - *ps2);
+}
