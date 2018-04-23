2ba1f605e1b85f69d7931c08ff1f17cc99801248
Zhipeng Wen <zwen@e1z3r3p3.42.us.org>
2018-04-22 19:36:18 -0700

    ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/22 18:59:19 by zwen              #+#    #+#             */
+/*   Updated: 2018/04/22 19:34:58 by zwen             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s)
+{
+	register char	*ret;
+	register char	*ptr;
+	register int	len;
+
+	if (!s)
+		return (NULL);
+	ptr = (char *)s;
+	while (ft_isspace(*ptr))
+		ptr++;
+	len = ft_strlen(ptr);
+	while (len && ft_isspace(ptr[len - 1]))
+		len--;
+	ret = (char *)malloc(sizeof(*ret) * (len + 1));
+	if (ret)
+	{
+		ret[len] = '\0';
+		while (len--)
+			ret[len] = ptr[len];
+		return (ret);
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 20:11:18 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/22 18:57:40 by zwen             ###   ########.fr       */
+/*   Updated: 2018/04/22 19:35:43 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 int		ft_isspace(char c);
