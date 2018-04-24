48e61f2d84b41de8219fc465db3a247535ad9713
Zhipeng Wen <zwen@e1z2r3p24.42.us.org>
2018-04-23 18:42:31 -0700

    ft_putchar_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 20:11:18 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/23 18:37:31 by zwen             ###   ########.fr       */
+/*   Updated: 2018/04/23 18:41:47 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,6 @@ void	ft_putendl(char const *s);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 void	ft_putnbr(int n);
+void	ft_putchar_fd(char c, int fd);
 int		ft_isspace(char c);
 #endif
