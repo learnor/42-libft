f218fd5047c80e8c19577790c2cf4bd540909def
Zhipeng Wen <zwen@e1z2r3p24.42.us.org>
2018-04-23 17:45:26 -0700

    ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/23 00:56:17 by zwen              #+#    #+#             */
+/*   Updated: 2018/04/23 17:40:47 by zwen             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	register int	i;
+	register int	len;
+	register char	*res;
+	register int	neg;
+
+	i = n;
+	len = i < 0 ? 2 : 1;
+	neg = i < 0 ? -1 : 1;
+	while (i / 10)
+	{
+		len++;
+		i /= 10;
+	}
+	res = (char *)malloc(sizeof(*res) * (len + 1));
+	if (!res)
+		return (NULL);
+	res[len] = '\0';
+	i = n;
+	while (len--)
+	{
+		res[len] = '0' + i % 10 * neg;
+		i /= 10;
+	}
+	res[0] = neg > 0 ? res[0] : '-';
+	return (res);
+}
