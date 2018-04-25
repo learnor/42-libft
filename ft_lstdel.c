d9072e1f2eea72fe53930cdc51a390c526686356
Zhipeng Wen <zwen@e1z2r5p4.42.us.org>
2018-04-25 02:00:11 -0700

    ft_lstdel/ft_lstadd

---
+++ ft_lstdel.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/25 01:07:22 by zwen              #+#    #+#             */
+/*   Updated: 2018/04/25 01:51:42 by zwen             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*tmp;
+
+	if (!alst || !*alst)
+		return ;
+	while (*alst)
+	{
+		tmp = *alst;
+		*alst = tmp->next;
+		del(tmp->content, tmp->content_size);
+		free(tmp);
+		tmp = NULL;
+	}
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 20:11:18 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/25 01:09:42 by zwen             ###   ########.fr       */
+/*   Updated: 2018/04/25 01:59:00 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,5 +83,7 @@ void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstadd(t_list **alst, t_list *new);
 int					ft_isspace(char c);
 #endif
