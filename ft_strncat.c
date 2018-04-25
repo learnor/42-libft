6ead46b5686e602d93841a903574b1ccf95e697b
z10n <z10n.w3n.42@gmail.com>
2018-04-25 12:19:36 -0700

    fix ft_strncat not terminate properly

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 11:08:13 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/24 23:18:31 by zwen             ###   ########.fr       */
+/*   Updated: 2018/04/25 12:18:31 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
 {
-	register size_t	len;
+	register size_t	ns1;
+	register size_t	ns2;
 
-	len = ft_strlen(s2);
-	ft_memcpy(s1 + ft_strlen(s1), s2, len < n ? len + 1 : n);
+	ns1 = ft_strlen(s1);
+	ns2 = ft_strlen(s2);
+	ft_memcpy(s1 + ns1, s2, n = ns2 < n ? ns2 : n);
+	s1[ns1 + n] = '\0';
 	return (s1);
 }
