b17995d49e43d9f1c3a7979b4f6703336783464d
z10n <z10n.w3n.42@gmail.com>
2018-05-28 01:12:17 -0700

    optimize ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 12:06:19 by zwen              #+#    #+#             */
-/*   Updated: 2018/04/19 12:30:18 by zwen             ###   ########.fr       */
+/*   Updated: 2018/05/17 03:59:48 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	ptr = s;
 	while (*ptr)
 		ptr++;
-	if (c == '\0')
+	if (c == '\0' || !ptr)
 		return ((char *)ptr);
 	while (ptr-- != s)
 		if (*ptr == c)
